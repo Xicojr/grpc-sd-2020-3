debdc390867300bafef61519f8af4a908390a21b
Francisco das Chagas Peres Júnior <francisco.peres@lsdi.ufma.br>
2020-07-13 20:21:19 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,9 @@
-// definição do caminho do arquivo proto
-const PROTO_PATH = "./car.proto";
+const PROTO_PATH = "./restaurante.proto";
 
 const grpc = require('grpc');
 
 const protoLoader = require('@grpc/proto-loader');
 
-// carregamento do arquivo proto e geração das definições
 const packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
     {keepCase: true,
@@ -15,43 +13,32 @@ const packageDefinition = protoLoader.loadSync(
      oneofs: true
     });
 
-// carregamento do código do serviço
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).car;
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).restaurante;
 
-// criação de um objeto cliente que se conecta ao serviço no
-// endereço 127.0.0.1, na porta 50051 e sem segurança
-const client = new protoDescriptor.ServicoCarro('127.0.0.1:50051',
+const client = new protoDescriptor.Servicocardapio('127.0.0.1:50051',
                                     grpc.credentials.createInsecure());
 
 
-// cliente chama o procedimento remoto "RegistrarCarro", passando
-// um carro como parâmetro. Ao receber o resultado, uma função de
-// callback é executada
-client.RegistrarCarro({modelo: "versa", marca: "nissan", cor: "cinza"}, function(err, response) {
-    // verifica se ocorreu algum erro na comunicação
+client.RegistrarCarro({item: "risole de frango", valor: 3.50}, function(err, response) {
+    
     if (err != null) {
         console.log("Ocorreu um erro invocando o procedimento RegistrarCarro");
         return;
     }
 
-    console.log("Versa registrado com sucesso");
+    console.log("Risole de frango registrado com sucesso");
 
-    // cliente chama o procedimento remoto "RegistrarCarro", passando
-    // um carro como parâmetro. Ao receber o resultado, uma função de
-    // callback é executada
-    client.RegistrarCarro({modelo: "gol", marca: "volkswagen", cor: "vermelho"}, function(err, response) {
-        // verifica se ocorreu algum erro na comunicação
+    client.registrarItemNoCardapio({item: "coxinha", valor: 2.50}, function(err, response) {
+        
         if (err != null) {
             console.log("Ocorreu um erro invocando o procedimento RegistrarCarro");
             return;
         }
 
-        console.log("Gol registrado com sucesso");
+        console.log("Coxinha registrado com sucesso");
 
-        // cliente chama o procedimento remoto "ListarCarros", sem parâmetros
-        // e obtêm a lista de carros como resultado
-        client.ListarCarros({}, function(err, response) {
-            const lista = response.carros;
+        client.ListarCardapio({}, function(err, response) {
+            const lista = response.ListaDoCardapio;
 
             console.log(lista);
         });
@@ -59,12 +46,12 @@ client.RegistrarCarro({modelo: "versa", marca: "nissan", cor: "cinza"}, function
     });
 });
 
-client.ListarCarros({}, function(err, response) {
+client.ListarCardapio({}, function(err, response) {
     if (err != null) {
-        console.log("Ocorreu um erro invocando o procedimento ListarCarros");
+        console.log("Ocorreu um erro invocando o procedimento ListarCardapio");
         return;
     }
 
-    console.log(" >>>>> Lista de carros: " + JSON.stringify(response.carros) );
+    console.log(" >>>>> Cardápio: " + JSON.stringify(response.ListaDoCardapio) );
 });
 
